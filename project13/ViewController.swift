f151e23f3d12f40b1a21f94e37264cd3df094973
Felipe Gil <felipe.gil@redspace.com>
2021-08-03 17:07:42 -0300

    [project13] implemented alert message and loading screen.

--- project13/ViewController.swift
+++ project13/ViewController.swift
@@ -7,8 +7,6 @@
 import CoreImage
 import UIKit
 //TODO
-#warning("loading screen")
-#warning("Make the save button show an error if there was no image in the image View")
 #warning("Make the Change Filter button change its title to show the name of the currently filter")
 #warning("Try more than 1 slider with different properties")
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -19,27 +17,52 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     var context: CIContext!
     var currentFilter: CIFilter!
     
+    let loadingActivityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.style = .large
+        indicator.color = .white
+        indicator.startAnimating()
+        indicator.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin, .flexibleTopMargin, .flexibleBottomMargin]
+        return indicator
+    }()
+    let blurEffectView: UIVisualEffectView = {
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.alpha = 0.9
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        return blurEffectView
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var newTitle: String = "InstaFilter"
+        let newTitle: String = "InstaFilter"
         title = newTitle
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        
         
         context = CIContext()
         currentFilter = CIFilter(name: "CISepiaTone")
         
     }
     @objc private func importPicture() {
-
-        let picker = UIImagePickerController()
-        picker.allowsEditing = true
-        picker.delegate = self
-        present(picker, animated: true)
+        startLoadingAnimation()
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            let picker = UIImagePickerController()
+            picker.allowsEditing = true
+            picker.delegate = self
+            self.present(picker, animated: true) {
+                self.stopLoadingAnimation()
+            }
+        }
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
         guard let image = info[.editedImage] as? UIImage else { return }
         dismiss(animated: true)
         currentImage = image
@@ -47,6 +70,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let beginImage = CIImage(image: currentImage)
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
         applyProcessing()
+        stopLoadingAnimation()
+        
     }
     
     @IBAction func changeFilter(_ sender: UIButton) {
@@ -74,12 +99,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         let beginImage = CIImage(image: currentImage)
         currentFilter.setValue(beginImage, forKey: kCIInputIntensityKey)
-       
         applyProcessing()
     }
     
     @IBAction func save(_ sender: UIButton) {
-        guard let image = imageView.image else { return }
+        guard let image = imageView.image else {
+            let alertController = UIAlertController(title: "Error", message: "Please selct an image", preferredStyle: .actionSheet)
+            alertController.addAction(UIAlertAction(title: "Ok", style: .destructive))
+            present(alertController, animated: true)
+            return
+        }
         UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError: contextInfo:)),nil)
     }
     
@@ -89,7 +118,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     private func applyProcessing() {
         let inputKeys = currentFilter.inputKeys
-       
+        
         if inputKeys.contains(kCIInputIntensityKey){
             currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
         }
@@ -102,7 +131,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if inputKeys.contains(kCIInputCenterKey) {
             currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey)
         }
-
+        
         guard let outputImage = currentFilter.outputImage else { return }
         
         if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
@@ -121,5 +150,27 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             alertController.addAction(UIAlertAction(title: "Ok", style: .default))
             present(alertController, animated: true)
         }
+    }
+    @objc private func startLoadingAnimation() {
+        
+        blurEffectView.translatesAutoresizingMaskIntoConstraints = false
+        loadingActivityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.addSubview(blurEffectView)
+        UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.addSubview(loadingActivityIndicator)
+        NSLayoutConstraint.activate([
+            
+            blurEffectView.topAnchor.constraint(equalTo: view.topAnchor),
+            blurEffectView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            blurEffectView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            blurEffectView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            loadingActivityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loadingActivityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    private func stopLoadingAnimation() {
+        blurEffectView.removeFromSuperview()
+        loadingActivityIndicator.removeFromSuperview()
     }
 }
