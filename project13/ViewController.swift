f508a789b49097b8ef7a5969ef3d4fe8b6a0c830
GilCND <fgilcnd@gmail.com>
2021-08-18 14:22:32 -0300

    [project13-01] fixed the filter bug

--- project13/ViewController.swift
+++ project13/ViewController.swift
@@ -62,11 +62,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
         guard let image = info[.editedImage] as? UIImage else { return }
         dismiss(animated: true)
-        let beginImage = CIImage(image: image)
+
+        applyProcessing(filterIntensity: 0.5, image: image)
         
-        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-        currentImage = image
-        applyProcessing(filterIntensity: 0.5)
         stopLoadingAnimation()
     }
     
@@ -96,17 +94,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             present(alertController, animated: true)
             return
         }
-        UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError: contextInfo:)),nil)
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveImageErrorChecking(_:didFinishSavingWithError: contextInfo:)),nil)
     }
     
     @IBAction func changeFilterIntensity(_ sender: UISlider) {
-        applyProcessing(filterIntensity: sender.value)
+        applyProcessing(filterIntensity: sender.value, image: currentImage!)
     }
     
-    private func applyProcessing(filterIntensity: Float) {
-        
-       guard let currentImage = currentImage, let currentFilter = currentFilter else { return }
+    private func applyProcessing(filterIntensity: Float, image: UIImage) {
+        currentImage = image
+        guard let currentImage = currentImage, let currentFilter = currentFilter else { return }
         let inputKeys = currentFilter.inputKeys
+        let beginImage = CIImage(image: image)
+        
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
         
         if inputKeys.contains(kCIInputIntensityKey){
             currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
@@ -132,7 +133,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     private func set(filter: CIFilterName) {
         currentFilter = CIFilter.make(filter)
         changeFilterButton.setTitle("\(filter)", for: .normal)
-        applyProcessing(filterIntensity: 0.5)
+        applyProcessing(filterIntensity: 0.5, image: currentImage!)
     }
 }
 enum CIFilterName: String, CaseIterable {
@@ -180,7 +181,7 @@ extension ViewController {
         blurEffectView.removeFromSuperview()
         loadingActivityIndicator.removeFromSuperview()
     }
-    @objc private func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+    @objc private func saveImageErrorChecking(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
         if let error =  error {
             let alertController = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
             alertController.addAction(UIAlertAction(title: "Ok", style: .default))
